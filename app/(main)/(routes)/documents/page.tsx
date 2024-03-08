853408a09d2707f91156db9aa599a00fa7f73299
Abhay Panchal <abpanchal951357@gmail.com>
2024-03-08 13:58:51 +0530

    chore: user settings added

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+import Image from "next/image";
 
 const DocumentsPage = () => {
-  return <div>DocumentsPage</div>;
+  const { user } = useUser();
+
+  return (
+    <div className="flex h-full  flex-col items-center justify-center">
+      <Image
+        src={"/empty.png"}
+        alt="empty"
+        height={"300"}
+        width={"300"}
+        className="dark:hidden"
+      />
+      <Image
+        src={"/empty-dark.png"}
+        alt="empty"
+        height={"300"}
+        width={"300"}
+        className=" hidden dark:block"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Jotion
+      </h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
+  );
 };
 
 export default DocumentsPage;
